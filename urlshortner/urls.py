d53a2fe5718af6d8259619d16d1ab50aafc2a623
userAryush <khatriaryush@gmail.com>
2026-06-07 15:55:07 +0545

    added login and register page

--- urlshortner/urls.py
+++ urlshortner/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from shortner import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shortener.urls')),
+    path('', include('shortner.urls')),
     path('', include('accounts.urls'))
 ]
