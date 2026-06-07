64b875cde775e9cc8406e2924d0b45e9acea9f53
userAryush <khatriaryush@gmail.com>
2026-06-07 12:00:03 +0545

    added custom user model

---
+++ accounts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db.models import EmailField, CharField
+# Create your models here.
+class User(AbstractUser):
+    email = EmailField(unique=True, max_length=254)
+    username = CharField(max_length=150, unique=True, blank=True, null=True)
+    
+
+    USERNAME_FIELD = 'email'
